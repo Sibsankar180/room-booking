2b9f5edabcac3289f40dfa67e3492f73aa48cc71
Sibsankar180 <gatestudy00@gmail.com>
2025-04-25 00:22:33 +0530

    changes

---
+++ config/db.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2");
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '@Sibu8167',
+    database: 'room_booking'
+});
+
+connection.connect((err)=>{
+    if(err){
+        console.error("error connctiong to MySQL: ",err.message);
+    }else{
+        console.log("Connected to MySQL DB");
+    }
+})
+
+module.exports = connection;
---
+++ server.js
@@ -0,0 +1,19 @@
+const express =require('express');
+const cors = require("cors");
+const db = require("./config/db");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = 5000;
+
+app.get('/',(req,res)=>{
+    res.send("room booking API is running!");
+});
+
+app.listen(PORT,()=>{
+    console.log(`server runnging on port ${PORT}`);
+});
+
+
