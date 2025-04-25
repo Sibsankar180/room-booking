60e79736648236a38fccdd7fbff26f57f7603ba7
Sibsankar180 <gatestudy00@gmail.com>
2025-04-25 23:55:11 +0530

    changes

--- config/db.js
+++ config/db.js
@@ -1,11 +1,11 @@
 const mysql = require("mysql2");
-require('dotenv').config();
+ 
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '@Sibu8167',
-    database: 'room_booking'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
 });
 
 connection.connect((err)=>{
