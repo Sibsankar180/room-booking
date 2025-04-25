1f4b5782a9c207747950d39106ffae1b90bcface
Sibsankar180 <gatestudy00@gmail.com>
2025-04-26 00:03:35 +0530

    ....

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
  
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    host: 'localhost',
+    user:   "root",
+    password: "@Sibu8167",
+    database: "room_booking"
 });
 
 connection.connect((err)=>{
