2988497108569b467b996df7a094cd3da763d749
Sibsankar180 <gatestudy00@gmail.com>
2025-04-27 14:26:46 +0530

    filter

--- models/roomModel.js
+++ models/roomModel.js
@@ -15,12 +15,10 @@ const getAllRooms = (callback) =>{
 
 const searchRooms = (filters, callback ) => {
     const{ location, minPrice, maxPrice, check_in, check_out } = filters;
+  
+  const sql = "  SELECT * FROM rooms  WHERE is_available = TRUE AND price BETWEEN ? AND ? AND location LIKE ?AND id NOT IN (SELECT room_id FROM bookings WHERE ((check_in <= ? AND check_out > ?) OR (check_in < ? AND check_out >= ?) OR(check_in >= ? AND check_out <= ?))) ";
 
-    const sql1 = 'select * from rooms where is_avilable = true and price between ? and ? and location';
-    const sql2 = 'like ? and id not in ( select room_id from bookings where ( ( check_in <= ? and check_out > ?) or (check_in < ? and check_out';
-    const sql3 =   '  >= ?) or (check_in >= ? and check_out <= ?)))';
-
-    const sql = sql1 + sql2 + sql3;
+    
 
     const values = [minPrice || 0, maxPrice || 10000, `%${location || ''}%` , check_in,check_in,check_out,check_out, check_in, check_out ];
 
