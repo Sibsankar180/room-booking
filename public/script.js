39c0ff33c890c7928d506c7333d02b62df258529
Sibsankar180 <gatestudy00@gmail.com>
2025-04-27 15:50:37 +0530

    updated

--- public/script.js
+++ public/script.js
@@ -1,22 +1,22 @@
-const searchForm = document.querySelector("#searchForm");
+  
 
-searchForm.addEventListener('submit', async(e) =>{
+  document.querySelector("#searchRoom").addEventListener('click', async(e) =>{
 
     e.preventDefault();
 
-    const location = document.querySelector("#location").value;
-    const minPrice = document.querySelector("#minPrice").value;
-    const maxprice = document.querySelector("#maxPrice").value;
-    const checkIn = document.querySelector("#checkIn").value;
-    const checkOut = document.querySelector("#checkOut").value;
+    const location = document.getElementById("location").value;
+    const minPrice = document.getElementById("minPrice").value;
+    const maxPrice = document.getElementById("maxPrice").value;
+    const checkIn = document.getElementById("checkIn").value;
+    const checkOut = document.getElementById("checkOut").value;
 
-    const res = await fetch(`/api/rooms/search?location=${location}&minPrice=${minPrice}&maxPrice=${maxPrice}&check_in=${chekIn}&checkOut=${check_out}`);
+    const res = await fetch(`/api/rooms/search?location=${location}&minPrice=${minPrice}&maxPrice=${maxPrice}&check_in=${checkIn}&checkOut=${checkOut}`);
 
     const data = await res.json();
 
     const results = document.querySelector("#roomResults");
 
-    results.innerHTML = ' ';
+    results.innerHTML = '';
 
     if(data.length === 0 ){
         results.innerHTML = "<p>No rooms found. </p>";
@@ -33,8 +33,7 @@ searchForm.addEventListener('submit', async(e) =>{
                <p> <strong> Location: </strong> ${room.location}</p>`;
 
         results.appendChild(div);
-
-        
+      
     });
 
     
